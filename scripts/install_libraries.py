80618bb183c40927c4b538cf5d0bbefd9535d408
Stephan Kantelberg <stephan.kantelberg@zuehlke.com>
2023-11-06 19:31:38 +0100

    added static library compiling

--- scripts/install_libraries.py
+++ scripts/install_libraries.py
@@ -1,7 +1,7 @@
 import os
 import argparse
 import subprocess
-
+import sys
 
 BASE_PATH = '/compiler-explorer'
 LIB_PATH = 'libs'
@@ -12,6 +12,10 @@ def parse_args():
     parser.add_argument('-n', '--name', required=True, help='Please provide a name for the library.')
     parser.add_argument('-u', '--url', required=True, help='Please provide a github url.')
     parser.add_argument('-v', '--versions', required=True, nargs='+', help='Please provide git tags for given library.')
+    if sys.version_info < (3, 9):
+        parser.add_argument('-i', '--install-lib', default=False, action='store_true', help='Will build and install the library')
+    else:
+        parser.add_argument('-i', '--install-lib', default=False, action=argparse.BooleanOptionalAction, help='Will build and install the library')
     return parser.parse_args()
 
 
@@ -32,6 +36,11 @@ if __name__ == '__main__':
     if not git_repo.endswith('.git'):
         git_repo = f'{git_repo}.git'
 
-    os.chdir(WHOLE_LIB_PATH)
     for version in args.versions:
+        os.chdir(WHOLE_LIB_PATH)
         subprocess.call(['git', 'clone', '--single-branch', '--branch', f'{version}', f'{git_repo}', f'{version}'])
+        if args.install_lib:
+            os.makedirs(f'{WHOLE_LIB_PATH}/{version}/build')
+            os.chdir(f'{WHOLE_LIB_PATH}/{version}/build')
+            subprocess.call(['cmake', '-DCMAKE_POSITION_INDEPENDENT_CODE=TRUE', '..'])
+            subprocess.call(['make', 'install'])
